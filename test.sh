cf91e566b75fdca47c9c77044efdb22378d3d10f
Austin Huang <im@austinhuang.me>
2022-01-23 17:06:26 -0500

    big update

--- test.sh
+++ test.sh
@@ -11,30 +11,30 @@ do
     Line=$(echo "$Line" | sed -Ee "s/\\\\\[(\\^[0-9]+)\\\\\]/\[\1\]/g" -Ee "s/\\\\\`/\`/g")
     if [[ $Line =~ \[([0-9a-z-]+\.[0-9a-z]+)\] ]]; then
         raw="${BASH_REMATCH[1]}"
-        base=$(curl -Ls -m 10 "https://$raw/.well-known/matrix/server" | jq '."m.server"' | sed s/\"//g)
+        base=$(curl -Ls -m 10 "https://$raw/.well-known/matrix/server" -A "https://joinmatrix.org/servers" | jq '."m.server"' | sed s/\"//g)
         if [[ -z "$base" ]] || [[ "$base" == "null" ]]; then
             DNS=$(dig "_matrix._tcp.$raw" SRV +short | sed -E 's/([0-9]+ ){2}//g')
             SRV_PORT=$(echo "$DNS" | grep -oE "^[0-9]+")
             SRV_DOMAIN=$(echo "$DNS" | grep -oE "([a-z0-9]+\.)+$" | sed 's/\.$//')
             if [[ -n "$SRV_DOMAIN" ]]; then
-                body=$(curl -Ls -m 10 "https://$SRV_DOMAIN/_matrix/federation/v1/version")
+                body=$(curl -Ls -m 10 "https://$SRV_DOMAIN/_matrix/federation/v1/version" -A "https://joinmatrix.org/servers")
                 test=$(echo $body | jq .server)
-                reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "https://$SRV_DOMAIN/_matrix/client/r0/register" -d '{}')
+                reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "https://$SRV_DOMAIN/_matrix/client/r0/register" -A "https://joinmatrix.org/servers" -d '{}')
                 if [[ -z "$test" ]] && [[ -n "$SRV_PORT" ]]; then
-                    body=$(curl -Ls -m 10 "https://$SRV_DOMAIN:$SRV_PORT/_matrix/federation/v1/version")
+                    body=$(curl -Ls -m 10 "https://$SRV_DOMAIN:$SRV_PORT/_matrix/federation/v1/version" -A "https://joinmatrix.org/servers")
                 fi
             else
-                body=$(curl -Ls -m 10 "https://$raw/_matrix/federation/v1/version")
-                reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "https://$raw/_matrix/client/r0/register" -d '{}')
+                body=$(curl -Ls -m 10 "https://$raw/_matrix/federation/v1/version" -A "https://joinmatrix.org/servers")
+                reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "https://$raw/_matrix/client/r0/register" -A "https://joinmatrix.org/servers" -d '{}')
                 test=$(echo $body | jq .server)
                 if [[ -z "$test" ]]; then
-                    body=$(curl -Ls -m 10 "https://$raw:8448/_matrix/federation/v1/version")
+                    body=$(curl -Ls -m 10 "https://$raw:8448/_matrix/federation/v1/version" -A "https://joinmatrix.org/servers")
                 fi
             fi
         else
-            body=$(curl -Ls -m 10 "https://$base/_matrix/federation/v1/version")
-            client=$(curl -Ls -m 10 "https://$raw/.well-known/matrix/client" | jq '."m.homeserver"."base_url"' | sed s/\"//g | sed "s/\/$//g")
-            reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "$client/_matrix/client/r0/register" -d '{}')
+            body=$(curl -Ls -m 10 "https://$base/_matrix/federation/v1/version" -A "https://joinmatrix.org/servers")
+            client=$(curl -Ls -m 10 "https://$raw/.well-known/matrix/client" -A "https://joinmatrix.org/servers" | jq '."m.homeserver"."base_url"' | sed s/\"//g | sed "s/\/$//g")
+            reg=$(curl -Ls -m 10 -X POST -H "accept: application/json" -H "Content-Type: application/json" "$client/_matrix/client/r0/register" -A "https://joinmatrix.org/servers" -d '{}')
         fi
         name=$(echo $body | jq .server.name | sed s/\"//g)
         version=$(echo $body | jq .server.version | sed s/\"//g)
